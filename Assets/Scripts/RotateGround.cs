3aea1941820a31e263f41cf196aeee5f05c63364
Lelouny <jonathan.vera.7e3@itb.cat>
2021-02-10 22:46:47 +0100

    Unity3dBasics- Game 0.1

---
+++ Assets/Scripts/RotateGround.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGround : MonoBehaviour
+  
+    
+{
+    [SerializeField] private float speed;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal")<0|| Input.GetAxis("Horizontal") > 0) {
+            transform.Rotate(0,0,-Input.GetAxis("Horizontal")*speed*Time.deltaTime);
+         
+}
+        if (Input.GetAxis("Vertical")<0|| Input.GetAxis("Vertical") > 0)
+        {
+            transform.Rotate(Input.GetAxis("Vertical")*speed*Time.deltaTime,0,0);
+
+        }
+        
+    }
+}
